46a8b3d5a9f6a97ebe3dcc643fd00d379dc0e0f4
Caio Braga <caiotabraga@hotmail.com>
2023-10-30 20:24:22 -0300

    Post method and model created

---
+++ ScheduleAPI/Controllers/UserController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ScheduleAPI.Models;
+
+namespace ScheduleAPI.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class UserController
+{
+    private static List<User> users = new List<User>();
+    [HttpPost]
+    public void AddUser(User user)
+    {
+        users.Add(user);
+        Console.WriteLine(user.UserName);
+        Console.WriteLine(user.Email);
+        Console.WriteLine(user.PhoneNumber);
+    }
+
+}
---
+++ ScheduleAPI/Models/User.cs
@@ -0,0 +1,9 @@
+﻿namespace ScheduleAPI.Models;
+
+public class User
+{
+    public string UserName { get; set; }
+    public string Email { get; set; }
+    public string PhoneNumber { get; set; }
+
+}
