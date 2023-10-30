b6a22b61ff13752e71451ba66831aaac72d7c561
Caio Braga <caiotabraga@hotmail.com>
2023-10-30 20:48:55 -0300

    Validation of Post method implemented and tested

--- ScheduleAPI/Models/User.cs
+++ ScheduleAPI/Models/User.cs
@@ -1,9 +1,18 @@
-﻿namespace ScheduleAPI.Models;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ScheduleAPI.Models;
 
 public class User
 {
+    [Required(ErrorMessage = "Name is required.")]
+    [MinLength(4, ErrorMessage = "The Name's length must be more than 4 characters"), MaxLength(50, ErrorMessage = "The Name's length can't be above 50 characters")]
     public string UserName { get; set; }
+
+    [Required(ErrorMessage = "Email is required.")]
+    [MinLength(10, ErrorMessage = "The Emails's length must be more than 10 characters"), MaxLength(50, ErrorMessage = "The Email's length can't be above 50 character")]
     public string Email { get; set; }
+
+    [MinLength(11, ErrorMessage = "The Emails's length must be 11 characters"), MaxLength(11, ErrorMessage = "The Email's length must be 11 characters")]
     public string PhoneNumber { get; set; }
 
 }
