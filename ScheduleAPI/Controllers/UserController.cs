b578452dd1e23adf135198d625ddd23bb4c912b8
Caio Braga <caiotabraga@hotmail.com>
2023-10-30 22:00:22 -0300

    Correcting return message if user doesn't exist and showing to the client where it can find the user created

--- ScheduleAPI/Controllers/UserController.cs
+++ ScheduleAPI/Controllers/UserController.cs
@@ -5,31 +5,32 @@ namespace ScheduleAPI.Controllers;
 
 [ApiController]
 [Route("[controller]")]
-public class UserController
+public class UserController : ControllerBase
 {
     private static List<User> users = new List<User>();
     private static int Id = 0;
 
     [HttpPost]
-    public void AddUser(User user)
+    public IActionResult AddUser([FromBody] User user)
     {
         user.Id = Id++;
         users.Add(user);
-        Console.WriteLine(user.UserName);
-        Console.WriteLine(user.Email);
-        Console.WriteLine(user.PhoneNumber);
+        return CreatedAtAction(nameof(GetUserById), new { id = user.Id}, user);
     }
 
     [HttpGet]
-    public IEnumerable<User> GetUsers()
+    public IEnumerable<User> GetUsers([FromQuery] int skip = 0, [FromQuery] int take = 100)
     {
-        return users;
+        return users.Skip(skip).Take(take);
     }
 
     [HttpGet("{id}")]
-    public User? GetUserById(int id)
+    public IActionResult? GetUserById(int id)
     {
-        return users.FirstOrDefault(user => user.Id == id);
+        var user =  users.FirstOrDefault(user => user.Id == id);
+
+        if (user == null) return NotFound("User doesn't exist!");
+        return Ok(user);
     }
 
 }
