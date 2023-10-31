afca227b4506eaec5b668db125d927af441b460a
Caio Braga <caiotabraga@hotmail.com>
2023-10-31 11:42:50 -0300

    Starting database configuration

---
+++ ScheduleAPI/Data/UserContext.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace ScheduleAPI.Data;
+
+	public class UserContext : DbContext
+	{
+		public UserContext(DbContextOptions<UserContext> opts) : base(opts)
+    {
+			
+		}
+	}
+
